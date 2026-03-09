47570b84d533b495488e5b80fdba3ade821b3cb8
ayushmanjilkar04 <ayushmanjilkar04@gmail.com>
2026-03-09 22:32:58 +0530

    final upadte in styling

--- Front-end/script.js
+++ Front-end/script.js
@@ -6,88 +6,95 @@ const password = document.getElementById("password");
 const title = document.getElementById("title");
 const deadline = document.getElementById("deadline");
 const taskList = document.getElementById("task-list");
+const btn = document.querySelector("#complete-btn");
 
 function register() {
-    fetch(`${API}/register`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            username: username.value,
-            password: password.value
-        })
-    })
-    .then(res => res.json())
-    .then(data => {
-        alert(data.message);
-        window.location.href = "login.html";
+  fetch(`${API}/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: username.value,
+      password: password.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      alert(data.message);
+      window.location.href = "login.html";
     });
 }
 
 function login() {
-    fetch(`${API}/login`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            username: username.value,
-            password: password.value
-        })
-    })
-    .then(res => res.json())
-    .then(data => {
-        if (data.token) {
-            localStorage.setItem("userId", parseJwt(data.token).id);
-            window.location.href = "index.html";
-        } else {
-            alert(data.message);
-        }
+  fetch(`${API}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: username.value,
+      password: password.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.token) {
+        localStorage.setItem("userId", parseJwt(data.token).id);
+        window.location.href = "index.html";
+      } else {
+        alert(data.message);
+      }
     });
 }
 
 function parseJwt(token) {
-    return JSON.parse(atob(token.split('.')[1]));
+  return JSON.parse(atob(token.split(".")[1]));
 }
 
 function addTask() {
-    fetch(`${API}/add-task`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            userId: localStorage.getItem("userId"),
-            title: title.value,
-            deadline: deadline.value
-        })
-    }).then(() => loadTasks());
+    if (!title.value || !deadline.value) {
+        alert("Please fill all fields");
+        return;
+    }
+  fetch(`${API}/add-task`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      userId: localStorage.getItem("userId"),
+      title: title.value,
+      deadline: deadline.value,
+    }),
+  }).then(() => loadTasks());
 }
 
 function loadTasks() {
-    fetch(`${API}/tasks/${localStorage.getItem("userId")}`)
-    .then(res => res.json())
-    .then(tasks => {
-        taskList.innerHTML = "";
-        tasks.forEach(task => {
-            taskList.innerHTML += `
-                <li>
+  fetch(`${API}/tasks/${localStorage.getItem("userId")}`)
+    .then((res) => res.json())
+    .then((tasks) => {
+      taskList.innerHTML = "";
+      tasks.forEach((task) => {
+        taskList.innerHTML += `
+                <div id="tasks"><li>
                     <b>${task.title}</b><br>
                     Deadline: ${task.deadline}<br>
                     Status: ${task.status}<br>
-                    <button onclick="completeTask('${task._id}')">Complete</button>
+                    <button id="complete-btn" onclick="completeTask('${task._id}')">Complete</button>
                     <button onclick="deleteTask('${task._id}')">Delete</button>
                 </li>
+                </div>
             `;
-        });
+      });
     });
 }
 
 function deleteTask(id) {
-    fetch(`${API}/delete-task/${id}`, { method: "DELETE" })
-    .then(() => loadTasks());
+  fetch(`${API}/delete-task/${id}`, { method: "DELETE" }).then(() =>
+    loadTasks(),
+  );
 }
 
 function completeTask(id) {
-    fetch(`${API}/update-status/${id}`, { method: "PUT" })
-    .then(() => loadTasks());
+  fetch(`${API}/update-status/${id}`, { method: "PUT" }).then(() =>
+    loadTasks(),
+  );
 }
-
 if (window.location.pathname.includes("index.html")) {
-    loadTasks();
+  loadTasks();
 }
